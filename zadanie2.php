1063eba30ab9fe98c5ff88412663ebb70a03c50e
BartoszBarszcz <90175781+BartoszBarszcz@users.noreply.github.com>
2021-09-14 07:54:23 +0200

    Create zadanie2.php

---
+++ zadanie2.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pl" dir="ltr">
+<head>
+  <meta charset="utf-8";
+</head>
+<body>
+<h1>TU</h1>
+<?php
+  include 'zadanie2_1.php';
+  require 'zadanie2_1.php';//nie wyświetla dalej pliku
+  include_once 'zadanie2_1.php';
+  require_once 'zadanie2_1.php';
+ ?>
+</body>
+<?php
+echo "AB"
+echo ,__FILE__,;
+
+ ?>
